feaabe3982e965740e1188e5720bf6f95cfeb9cc
coderAndrew <omolloandrew37@gmail.com>
2025-04-05 11:53:30 +0300

    parameter properties

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,34 @@
 "use strict";
-let quantity = 50;
-function greet(name) {
-    return name ? `Hello, ${name}!` : "Hola, amigo!";
+class Account {
+    constructor(id, name, email, _balance) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this._balance = _balance;
+    }
+    deposit(amount) {
+        if (amount > 0) {
+            this._balance += amount;
+            console.log(`Deposited ${amount}. New balance is ${this._balance}`);
+        }
+        else {
+            console.log(`Deposit amount must be positive`);
+        }
+    }
+    getBalance() {
+        return this._balance;
+    }
+    _calculateTax() {
+        return this._balance * 0.2;
+    }
+    getTax() {
+        return this._calculateTax();
+    }
 }
-function getCustomer(id) {
-    return id === 1 ? { birthdate: new Date() } : null;
-}
-console.log(getCustomer(1));
-console.log(getCustomer(2));
+let account = new Account(1, "John Doe", "2g5wI@example.com", 1000);
+account.deposit(500);
+console.log(account.getTax());
+console.log(typeof account);
+const balance = account.getBalance();
+console.log(balance);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
-let speed: number | null = null;
+class Account {
+  constructor(
+    public readonly id: number,
+    public name: string,
+    public email: string,
+    private _balance: number
+  ) {}
 
-let ride = {
-  speed: speed ?? 30,
-};
+  deposit(amount: number): void {
+    if (amount > 0) {
+      this._balance += amount;
+      console.log(`Deposited ${amount}. New balance is ${this._balance}`);
+    } else {
+      console.log(`Deposit amount must be positive`);
+    }
+  }
 
-let phone = document.querySelector("#phone") as HTMLInputElement;
+  getBalance(): number {
+    return this._balance;
+  }
+
+  private _calculateTax(): number {
+    return this._balance * 0.2; // Example tax calculation
+  }
+
+  getTax(): number {
+    return this._calculateTax();
+  }
+}
+
+let account = new Account(1, "John Doe", "2g5wI@example.com", 1000);
+account.deposit(500);
+console.log(account.getTax()); // account.getTax(); // 300
+
+console.log(typeof account); // object
+const balance = account.getBalance();
+console.log(balance); // 1500
